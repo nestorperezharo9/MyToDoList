44b7eec334456bd869dabc0c9fbf6698c6fc9b72
nestor <nestorperezharo9@gmail.com>
2021-04-09 11:37:22 +0100

    crud

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,10 +8,35 @@ import { TodoService } from './share/todo.service';
   providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray : any[];
+  constructor(private toDoService: TodoService) { }
 
   ngOnInit(): void {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+      this.toDoListArray.sort((a,b) => {
+        return a.isChecked - b.isChecked;
+      })
+    });
+  }
+
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
+
+  alterCheck($key: string,isChecked) {
+    this.toDoService.checkOrUnCheckTitle($key,!isChecked);
+  }
+
+  onDelete($key : string){
+    this.toDoService.removeTitle($key);
   }
 
 }
